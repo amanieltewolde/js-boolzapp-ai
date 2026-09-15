102f177d1b0547951a99471ef7e6606bb83f88c6
amanieltewolde <aman94@hotmail.it>
2026-09-15 17:15:35 +0200

    preparing JS: firsts DOM refs + set up messagges structure as server's data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 console.log('Facciamoci una chiacchera')
 // set up endpoint gemini
-const endpoint = `${geminiConfig.endpoint}?key=${geminiConfig.apiKey};`
+const endpoint = `${geminiConfig.endpoint}?key=${geminiConfig.apiKey}`;
+
+// DOM Ref
+const chatBoxEl = document.querySelector('.chat-box');
+const chatFormEl = document.getElementById('chat-form');
+const chatInputEl = chatFormEl.querySelector('input')
+
+// storia conversazione 
+const messagges = [
+    {
+        text:'ciao sono Ama, come stai?',
+        type:'sent',
+        time:'15/09/26 14:57',
+    },   
+    {
+        text:'io sto molto bene, tu?',
+        type:'received',
+        time:'15/09/26 15:00',
+    }
+];
